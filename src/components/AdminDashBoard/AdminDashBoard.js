f3d826ea6f4e766cdbd60d2db4fcce5105ac418d
Kelindi <kelindi@Kelindis-MacBook-Pro.local>
2021-11-05 02:35:44 -0400

    created admin dashboard table

--- src/components/AdminDashBoard/AdminDashBoard.js
+++ src/components/AdminDashBoard/AdminDashBoard.js
@@ -17,23 +17,52 @@ class AdminDashBoard extends Component {
         
         return (
             <div className="font-sans">
-                {transactions.map((transaction) =>
-                    <div key={transaction.id} className = 'w-full'>
+                <table className = "table-auto w-full">
+                    <thead>
+                        <tr>
+                            <th className = "px-4 py-2 border text-center">Origin
+                                <input className = "border rounded-md"></input>
+                            </th>
+                            <th className = "px-4 py-2 border text-center">Destination
+                                <input className = "border rounded-md"></input>
+                            </th>
+                            <th className = "px-4 py-2 border text-center">Amount
+                                <input className = "border rounded-md"></input>
+                            </th>
+                            <th className = "px-4 py-2 border text-center">Date
+                                <input className = "border rounded-md"></input>
+                            </th>
+                            <th className = "px-4 py-2 border text-center">Time
+                                <input className = "border rounded-md"></input>
+                            </th>
+                            <th className = "px-4 py-2 border text-center">ID
+                                <input className = "block self-center border rounded-md m-auto"></input>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {transactions.map((transaction) =>{
+                        return(
+                        <tr key = {transaction.id}>
+                            <td className = "px-4 py-2 border text-center">{transaction.originUserName.userName.toString()}</td>
+                            <td className = "px-4 py-2 border text-center">{transaction.destinationUserName.userName.toString()}</td>
+                            <td className = "px-4 py-2 border text-center">{transaction.amount.toString()}</td>
+                            <td className = "px-4 py-2 border text-center">{transaction.date.toString()}</td>
+                            <td className = "px-4 py-2 border text-center">{transaction.time.toString()}</td>
+                            <td className = "px-4 py-2 border text-center">{transaction.id.toString()}</td>
+                        </tr>
+                        )
+                        })}
+                    </tbody>
+                </table>    
+                       
+      
                     
-                        <div className = "inline w-1/7">{transaction.originUserName.userName.toString()}</div>
-                        <div className = "inline w-1/7">{transaction.destinationUserName.userName.toString()}</div>
-                        <div className = "inline w-1/7">{transaction.amount.toString()}</div>
-                        <div className = "inline w-1/7">{transaction.date.toString()}</div>
-                        <div className = "inline w-1/7">{transaction.time.toString()}</div>
-                        <div className = "inline w-1/7">{transaction.id.toString()}</div>
                     
-                    
-                    </div>
-                    
-      )}
             </div>
           );
-    }
+    
+}
 }
  
 export default AdminDashBoard;
