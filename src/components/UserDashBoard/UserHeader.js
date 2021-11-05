741fb906849fe84592b7c08e5195bbf30cf19b30
Anujj <anujjbubna39@gmail.com>
2021-11-05 14:01:08 -0400

    Set up structure for User Header

--- src/components/UserDashBoard/UserHeader.js
+++ src/components/UserDashBoard/UserHeader.js
@@ -12,7 +12,14 @@ class UserHeader extends Component {
         
         return (
             <div className = "h-1/5 bg-purple-300">
-                
+                {/* 1) placeholder profile photo which can be modified by user
+                    2) Display Name
+                    3) Display Account Balance 
+                    4) 3 buttons:
+                        - Send
+                        - Request
+                        - Split*/}
+                {console.log(this.props.currentUser)}
             </div>
           );
     }
