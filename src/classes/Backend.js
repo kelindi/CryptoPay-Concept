1eee26b3f7a291611553fb7f605ee050597d62ee
Kelindi <kelindi@Kelindis-MacBook-Pro.local>
2021-11-05 21:40:51 -0400

    added option to add users and edit user info  in admindashboard

--- src/classes/Backend.js
+++ src/classes/Backend.js
@@ -12,10 +12,10 @@ class Backend {
         this.u3 = uuid()
         this.u4 = uuid()
         this.adminID = uuid()
-        this.user1 = new User('firstName','lastName','user',1001,'wallet1')
-        this.user2 = new User('firstName2','lastName2','user2',2002,'wallet2')
-        this.user3 = new User('firstName3','lastName3','user3',3003,'wallet3')
-        this.user4 = new User('firstName4','lastName4','user4',4004,'wallet4')
+        this.user1 = new User('firstName','lastName','user',1001,uuid())
+        this.user2 = new User('firstName2','lastName2','user2',2002,uuid())
+        this.user3 = new User('firstName3','lastName3','user3',3003,uuid())
+        this.user4 = new User('firstName4','lastName4','user4',4004,uuid())
         this.admin = new Admin('adminFirst','adminLast')
         this.loginDB = {'user':'user','user2':'user2','user3':'user3','user4':'user4','admin':'admin'}
         this.tokenDB = {'user':this.u1,'user2':this.u2,'user3':this.u3,'user4':this.u4,'admin':this.adminID}
