e6d474141e32a563d69bc5596fce1bde983761e7
Kelindi <ayushk2020@gmail.com>
2021-11-07 20:53:30 -0500

    m

--- src/components/UserDashBoard/UserFeed/TransactionFeed/OutgoingMoneyTransaction.js
+++ src/components/UserDashBoard/UserFeed/TransactionFeed/OutgoingMoneyTransaction.js
@@ -19,25 +19,34 @@ class OutgoingMoneyRequest extends Component{
         const {request, changeOutgoingMoneyRequests, global} = this.props
 
         return(
-            <div className="relative rounded-2xl w-full h-20 bg-gray-100 mt-2">
-                <div className="absolute float-left rounded-full h-20 w-20">
-                    <img className="float-left absolute rounded-full h-20 w-20 flex px-3 py-3 items-center
-                    justify-center" src={request.destinationUser.profilePicture}></img>
-                </div>
-                <div className="relative float-right top-2 w-10/12">
-                    <div className="float-left tracking-wide text-center"><b>{request.destinationUser.firstName}</b> {request.destinationUser.lastName}     <small>({request.destinationUser.userName})</small></div> <br/>
-                    <div className="float-left">
-                        <div className="px-1 float-left font-light">Amount:{request.amount} </div>
-                        <div className="px-1 font-light float-right"> Sent on {request.date} </div>
-                    </div>
-                    <div className="float-right">
-                        <button className="float-left bg-red-600 px-2 py-1 rounded-md"
-                                onClick={() => this.handleRescind(request)}>Rescind</button>
-                    </div>
-                
-                </div>
-                
+        <div className="h-12 flex items-center px-4 py-3 border-b bg-gray-100 rounded-xl shadow-md my-2">
+          <img
+            className="h-8 w-8 rounded-full object-cover mx-1"
+            src={request.originUser.profilePicture}
+          />
+          <p className="text-gray-600 text-sm mx-2">
+            <span className="font-bold block">
+            <strong className = "uppercase">({request.originUser.userName}) </strong>
+            <span>{request.originUser.firstName}</span>{" "}
+            <span>{request.originUser.lastName}</span>
+            </span>
+            
+            <div className="px-1 float-left font-light">
+              <strong>Amount:</strong>{request.amount}{" "}
             </div>
+            <div className="px-1 font-light float-right">
+              {" "}
+              Sent on {request.date}{" "}
+            </div>
+          </p>
+          <div className="ml-auto text-xs">
+            <button
+              className="mx-1 px-2 py-1 bg-red-500 rounded-3xl text-white shadow-lg"
+              onClick={() => this.handleRescind(request)}>
+              Rescind
+            </button>
+            </div>
+        </div>
         )
     }
 }
