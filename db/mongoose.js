e979dfe7aa35aea027976ff6529e99e94dcf7687
Kelindi <ayushk2020@gmail.com>
2021-12-03 15:53:41 -0500

    mongofix

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://CryptoPay:Team42@cluster0.brbjl.mongodb.net/test'
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://CryptoPay:Team42@cluster0.brbjl.mongodb.net'
 
 mongoose.connect(mongoURI,
-    {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+    {useNewUrlParser: true, useUnifiedTopology: true})
     .catch((error) => {
+        console.log(error)
         console.log('Error connecting to mongodb. Timeout reached.')
     });
 
     module.exports = { mongoose }
+
+
+
+    
+    
