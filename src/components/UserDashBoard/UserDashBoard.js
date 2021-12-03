c33e156ebc546ff62410663a3cc74f0c6116f94d
Kelindi <ayushk2020@gmail.com>
2021-12-02 22:49:55 -0500

    added balance from metamask

--- src/components/UserDashBoard/UserDashBoard.js
+++ src/components/UserDashBoard/UserDashBoard.js
@@ -1,88 +1,133 @@
-import React, { Component } from 'react';
-import FriendsList from './FriendsList';
-import NotificationBar from './NotificationBar/NotificationBar';
-import UserFeed from './UserFeed/UserFeed';
-import UserHeader from './UserHeader/UserHeader';
-
+import React, { Component } from "react";
+import FriendsList from "./FriendsList";
+import NotificationBar from "./NotificationBar/NotificationBar";
+import UserFeed from "./UserFeed/UserFeed";
+import UserHeader from "./UserHeader/UserHeader";
+import { ethers } from "ethers";
 
 class UserDashBoard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userBalance: this.props.currentUser.currentAccountBalance,
-            userName: this.props.currentUser.userName,
-            firstName: this.props.currentUser.firstName,
-            lastName: this.props.currentUser.lastName,
-            profilePicture: this.props.currentUser.profilePicture,
-            friendsList: this.props.currentUser.friends,
-            incomingFriendRequests: this.props.currentUser.friendRequests,
-            sentFriendRequests: this.props.currentUser.sentFriendRequests,
-            incomingMoneyRequests: this.props.currentUser.requests,
-            sentMoneyRequests: this.props.currentUser.sentRequests,
-            transactions: this.props.backend.transactions
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userBalance: this.props.currentUser.currentAccountBalance,
+      userName: this.props.currentUser.userName,
+      firstName: this.props.currentUser.firstName,
+      lastName: this.props.currentUser.lastName,
+      profilePicture: this.props.currentUser.profilePicture,
+      friendsList: this.props.currentUser.friends,
+      incomingFriendRequests: this.props.currentUser.friendRequests,
+      sentFriendRequests: this.props.currentUser.sentFriendRequests,
+      incomingMoneyRequests: this.props.currentUser.requests,
+      sentMoneyRequests: this.props.currentUser.sentRequests,
+      transactions: this.props.backend.transactions,
+      provider: null,
+      signer: null,
+      wallet:null,
 
-    //comment
-    changeUserBalance = (x) => {
-        this.setState({userBalance:x})
-    }
+    };
+  }
+  componentDidMount = () => {
+    this.setUpWeb3()
+    
+  };
 
-    changeUserName = (x) => {
-        this.setState({userName:x})
-    }
+  setUpWeb3 = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const wallet = await signer.getAddress()
+    let userBalance = await provider.getBalance(wallet)
+    userBalance = ethers.utils.formatEther(userBalance)
 
-    changeFirstName = (x) => {
-        this.setState({FirstName:x})
-    }
+    this.setState({provider:provider,signer:signer,wallet:wallet,userBalance:userBalance})
 
-    changeLastName = (x) => {
-        this.setState({LastName:x})
-    }
+  };
+  //comment
+  changeUserBalance = (x) => {
+    this.setState({ userBalance: x });
+  };
 
-    changeProfilePicture = (x) => {
-        this.setState({profilePicture:x})
-    }
+  changeUserName = (x) => {
+    this.setState({ userName: x });
+  };
 
-    changeFriendsList = (x) => {
-        this.setState({friendsList:x})
-    }
+  changeFirstName = (x) => {
+    this.setState({ FirstName: x });
+  };
 
-    changeIncomingFriendRequests = (x) => {
-        this.setState({incomingFriendRequests:x})
-    }
+  changeLastName = (x) => {
+    this.setState({ LastName: x });
+  };
 
-    changeSentFriendRequests = (x) => {
-        this.setState({sentFriendRequests:x})
-    }
+  changeProfilePicture = (x) => {
+    this.setState({ profilePicture: x });
+  };
 
-    changeIncomingMoneyRequests = (x) => {
-        this.setState({incomingMoneyRequests:x})
-    }
+  changeFriendsList = (x) => {
+    this.setState({ friendsList: x });
+  };
 
-    changeSentMoneyRequests = (x) => {
-        this.setState({sentMoneyRequests:x})
-    }
+  changeIncomingFriendRequests = (x) => {
+    this.setState({ incomingFriendRequests: x });
+  };
 
-    render() {
-        const {currentUser} = this.props
-        return (
-            <div className="font-sans">
-                <div className = "flex flex-column h-100">
-                    
-                    <div className = "w-10/12 h-screen flex-shrink-0">
-                        <NotificationBar changeFriendsList = {this.changeFriendsList} changeIncomingFriendRequests = {this.changeIncomingFriendRequests} global = {this.state}></NotificationBar>
-                        <UserHeader changeSentMoneyRequests = {this.changeSentMoneyRequests} changeUserBalance = {this.changeUserBalance} global = {this.state} backend ={this.props.backend} currentUser ={this.props.currentUser}></UserHeader>
+  changeSentFriendRequests = (x) => {
+    this.setState({ sentFriendRequests: x });
+  };
 
-                        <UserFeed changeSentFriendRequests = {this.changeSentFriendRequests} global = {this.state} changeOutgoingMoneyRequests = {this.changeSentMoneyRequests} changeIncomingMoneyRequests = {this.changeIncomingMoneyRequests} changeUserBalance = {this.changeUserBalance} changeFriendsList = {this.changeFriendsList} changeIncomingFriendRequests = {this.changeIncomingFriendRequests} backend ={this.props.backend} currentUser ={this.props.currentUser}></UserFeed>
-                    </div>
-                    
-                    <FriendsList changeUserBalance = {this.changeUserBalance} global = {this.state} changeSentMoneyRequests = {this.changeSentMoneyRequests} className = "w-2/12 flex-shrink-0" global = {this.state} changeSentFriendRequests = {this.changeSentFriendRequests} backend ={this.props.backend} currentUser ={this.props.currentUser}></FriendsList>
-                </div>
-                
-            </div>
-          );
-    }
+  changeIncomingMoneyRequests = (x) => {
+    this.setState({ incomingMoneyRequests: x });
+  };
+
+  changeSentMoneyRequests = (x) => {
+    this.setState({ sentMoneyRequests: x });
+  };
+
+  render() {
+    const { currentUser } = this.props;
+    return (
+      <div className="font-sans">
+        <div className="flex flex-column h-100">
+          <div className="w-10/12 h-screen flex-shrink-0">
+            <NotificationBar
+              changeFriendsList={this.changeFriendsList}
+              changeIncomingFriendRequests={this.changeIncomingFriendRequests}
+              global={this.state}
+            ></NotificationBar>
+            <UserHeader
+              changeSentMoneyRequests={this.changeSentMoneyRequests}
+              changeUserBalance={this.changeUserBalance}
+              global={this.state}
+              backend={this.props.backend}
+              currentUser={this.props.currentUser}
+            ></UserHeader>
+
+            <UserFeed
+              changeSentFriendRequests={this.changeSentFriendRequests}
+              global={this.state}
+              changeOutgoingMoneyRequests={this.changeSentMoneyRequests}
+              changeIncomingMoneyRequests={this.changeIncomingMoneyRequests}
+              changeUserBalance={this.changeUserBalance}
+              changeFriendsList={this.changeFriendsList}
+              changeIncomingFriendRequests={this.changeIncomingFriendRequests}
+              backend={this.props.backend}
+              currentUser={this.props.currentUser}
+            ></UserFeed>
+          </div>
+
+          <FriendsList
+            changeUserBalance={this.changeUserBalance}
+            global={this.state}
+            changeSentMoneyRequests={this.changeSentMoneyRequests}
+            className="w-2/12 flex-shrink-0"
+            global={this.state}
+            changeSentFriendRequests={this.changeSentFriendRequests}
+            backend={this.props.backend}
+            currentUser={this.props.currentUser}
+          ></FriendsList>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default UserDashBoard;
--- src/components/UserDashBoard/UserHeader/UserHeader.js
+++ src/components/UserDashBoard/UserHeader/UserHeader.js
@@ -1,122 +1,157 @@
-import React, { Component } from 'react';
-import SendPopUp from './SendPopUp'; 
-import RequestPopUp from './RequestPopUp'
-import SplitPopUp from './SplitPopUp';
+import React, { Component } from "react";
+import SendPopUp from "./SendPopUp";
+import RequestPopUp from "./RequestPopUp";
+import SplitPopUp from "./SplitPopUp";
 
 class UserHeader extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { 
-            firstName: this.props.global.firstName,
-            lastName: this.props.global.lastName,
-            userName: this.props.global.userName,
-            pf: this.props.global.profilePicture,
-            balance: this.props.global.userBalance,
-            currentUser: this.props.currentUser,
-            // used backend.js
-            sendOpen: false,
-            requestOpen: false,
-            splitOpen: false,
-        } 
-        this.changeBalance = this.changeBalance.bind(this)
-    }
+    this.state = {
+      firstName: this.props.global.firstName,
+      lastName: this.props.global.lastName,
+      userName: this.props.global.userName,
+      pf: this.props.global.profilePicture,
+      balance: this.props.global.userBalance,
+      currentUser: this.props.currentUser,
+      // used backend.js
+      sendOpen: false,
+      requestOpen: false,
+      splitOpen: false,
+    };
+    this.changeBalance = this.changeBalance.bind(this);
+  }
+  
+  sendPopOn = () => {
+    this.setState({
+      sendOpen: true,
+    });
+  };
 
-    sendPopOn = () => {
-        this.setState({
-            sendOpen: true,
-        });
-    }
+  sendPopOff = () => {
+    this.setState({
+      sendOpen: false,
+    });
+  };
 
-    sendPopOff = () => {
-        this.setState({
-            sendOpen: false,
-        });
-    }
+  reqPop = () => {
+    this.setState({
+      requestOpen: !this.state.requestOpen,
+    });
+  };
 
-    reqPop = () => {
-        this.setState({
-            requestOpen: !this.state.requestOpen,
-        });
-    }
+  reqPopOff = () => {
+    this.setState({
+      requestOpen: false,
+    });
+  };
 
-    reqPopOff = () => {
-        this.setState({
-            requestOpen: false,
-        });
-    }
+  splitPopOn = () => {
+    this.setState({
+      splitOpen: true,
+    });
+  };
 
-    splitPopOn = () => {
-        this.setState({
-            splitOpen: true,
-        });
-    }
+  splitPopOff = () => {
+    this.setState({
+      splitOpen: false,
+    });
+  };
 
-    splitPopOff = () => {
-        this.setState({
-            splitOpen: false,
-        });
-    }
+  changeBalance(deduction) {
+    const newAmount = this.props.global.userBalance - deduction;
+    this.props.changeUserBalance(newAmount);
+  }
 
-    changeBalance(deduction) {
-        const newAmount = this.props.global.userBalance - deduction
-        this.props.changeUserBalance(newAmount)
-    }
+  render() {
+    const { changeSentMoneyRequests } = this.props;
 
-
-    render() {
-        const { changeSentMoneyRequests } = this.props
-
-        return (
-            <div className = "flex flex-row h-1/5 bg-white mt-2 mb-2 rounded-xl shadow-2xl">
-                {/* 1) placeholder profile photo which can be modified by user
+    return (
+      <div className="flex flex-row h-1/5 bg-white mt-2 mb-2 rounded-xl shadow-2xl">
+        {/* 1) placeholder profile photo which can be modified by user
                     2) Display Name
                     3) Display Account Balance 
                     4) 3 buttons:
                         - Send
                         - Request
                         - Split*/}
-                {/* <img className='UserProfilePhoto' src={this.state.pF} alt="Profile Photo"/> */}
-                <div className='w-48 h-48 flex-shrink-0'>
-                    <img className='shadow-2xl rounded-full h-2/3 w-2/3 m-4' src={this.state.pf} alt="Profile Photo"/>
-                </div>
-                
-                <div className='text-left flex flex-col flex-shrink-0'>
-                    {/* Info */}
-                    <div className = "py-2 text-4xl"><b>{this.props.global.userName}</b></div>
-                    <div className='py-2 text-2xl'>{this.state.firstName} {this.props.global.lastName}</div>
-                    <div className='py-2 text-xl'><b>Balance:</b>{this.props.global.userBalance}</div>
+        {/* <img className='UserProfilePhoto' src={this.state.pF} alt="Profile Photo"/> */}
+        <div className="w-48 h-48 flex-shrink-0">
+          <img
+            className="shadow-2xl rounded-full h-2/3 w-2/3 m-4"
+            src={this.state.pf}
+            alt="Profile Photo"
+          />
+        </div>
 
-                </div>
+        <div className="text-left flex flex-col flex-shrink-0">
+          {/* Info */}
+          <div className="py-2 text-4xl">
+            <b>{this.props.global.userName}</b>
+          </div>
+          <div className="py-2 text-2xl">
+            {this.state.firstName} {this.props.global.lastName}
+          </div>
+          <div className="py-2 text-xl">
+            <b>Balance:</b>
+            {this.props.global.userBalance}
+          </div>
+        </div>
 
-                <div className='ml-auto px-10 my-1 text-white'>
-                    {/* Buttons */}
-                    <button className='w-24 h-10 my-1 bg-blue-500 hover:bg-green-500 rounded-3xl shadow-lg' 
-                            onClick={this.sendPopOn}>Send</button><br></br>
-                    <button className='w-24 h-10 my-1 bg-blue-500 rounded-3xl shadow-lg hover:bg-green-500'
-                            onClick={this.reqPop}>Request</button><br></br>
-                    <button className='w-24 h-10 my-1 bg-blue-500 rounded-3xl shadow-lg hover:bg-green-500'  
-                            onClick={this.splitPopOn}>Split</button>
-                </div>
-                {this.state.sendOpen ? <SendPopUp currentUser={this.state.currentUser} 
-                                                friendsList = {this.props.global.friendsList}
-                                                  updateBalance={this.changeBalance} 
-                                                  minimizeSend={this.sendPopOff}
-                                                  maximizeSend={this.sendPopOn} /> : null}
-                {this.state.requestOpen ? <RequestPopUp toggle={this.reqPop} 
-                                                        friendsList = {this.props.global.friendsList} 
-                                                        minimizeSend={this.reqPopOff}
-                                                        maximizeSend={this.sendPopOn}
-                                                        changeSentMoneyRequests={this.props.changeSentMoneyRequests}
-                                                        global={this.props.global}/> : null}
-                {this.state.splitOpen ? <SplitPopUp currentUser={this.state.currentUser} 
-                                                //   updateBalance={this.changeBalance} 
-                                                  minimizeSplit={this.splitPopOff}
-                                                  updateBalance={this.changeBalance}
-                                                  maximizeSplit={this.splitPopOn} /> : null}
-            </div>
-          );
-    }
+        <div className="ml-auto px-10 my-1 text-white">
+          {/* Buttons */}
+          <button
+            className="w-24 h-10 my-1 bg-blue-500 hover:bg-green-500 rounded-3xl shadow-lg"
+            onClick={this.sendPopOn}
+          >
+            Send
+          </button>
+          <br></br>
+          <button
+            className="w-24 h-10 my-1 bg-blue-500 rounded-3xl shadow-lg hover:bg-green-500"
+            onClick={this.reqPop}
+          >
+            Request
+          </button>
+          <br></br>
+          <button
+            className="w-24 h-10 my-1 bg-blue-500 rounded-3xl shadow-lg hover:bg-green-500"
+            onClick={this.splitPopOn}
+          >
+            Split
+          </button>
+        </div>
+        {this.state.sendOpen ? (
+          <SendPopUp
+            currentUser={this.state.currentUser}
+            friendsList={this.props.global.friendsList}
+            updateBalance={this.changeBalance}
+            minimizeSend={this.sendPopOff}
+            maximizeSend={this.sendPopOn}
+          />
+        ) : null}
+        {this.state.requestOpen ? (
+          <RequestPopUp
+            toggle={this.reqPop}
+            friendsList={this.props.global.friendsList}
+            minimizeSend={this.reqPopOff}
+            maximizeSend={this.sendPopOn}
+            changeSentMoneyRequests={this.props.changeSentMoneyRequests}
+            global={this.props.global}
+          />
+        ) : null}
+        {this.state.splitOpen ? (
+          <SplitPopUp
+            currentUser={this.state.currentUser}
+            //   updateBalance={this.changeBalance}
+            minimizeSplit={this.splitPopOff}
+            updateBalance={this.changeBalance}
+            maximizeSplit={this.splitPopOn}
+          />
+        ) : null}
+      </div>
+    );
+  }
 }
- 
+
 export default UserHeader;
