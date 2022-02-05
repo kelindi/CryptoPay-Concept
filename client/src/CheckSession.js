f1484770f62723c644defe7261adfd139e1ccd07
Kelindi <ayushk2020@gmail.com>
2022-02-04 19:47:47 -0500

    changed server

--- client/src/CheckSession.js
+++ client/src/CheckSession.js
@@ -5,7 +5,7 @@ import Admin from "./classes/Admin";
 
 export const logout = (app) => {
     const url = `https://crypt0pay.herokuapp.com/users/logout`
-    //const url = `http://localhost:5000/users/logout`
+    const url = `http://localhost:5000/users/logout`
 
     fetch(url)
         .then(res => {
@@ -20,7 +20,7 @@ export const logout = (app) => {
 
 export const checkSession = async (app) => {
     const url = `https://crypt0pay.herokuapp.com/users/check-session`
-    //const url = `http://localhost:5000/users/check-session`
+    const url = `http://localhost:5000/users/check-session`
     try{
         let res = await fetch(url);
         if (res.status === 401) {
